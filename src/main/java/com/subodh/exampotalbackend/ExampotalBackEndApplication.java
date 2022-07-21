e3a6299b076c0fd59ce8a91d20f541f7de160259
Subodh Bhandari <subodh.bhandari@sbsolutionsnepal.com>
2022-07-21 11:40:15 +0545

    start

---
+++ src/main/java/com/subodh/exampotalbackend/ExampotalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.subodh.exampotalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampotalBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampotalBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/subodh/exampotalbackend/ExampotalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subodh.exampotalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampotalBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
