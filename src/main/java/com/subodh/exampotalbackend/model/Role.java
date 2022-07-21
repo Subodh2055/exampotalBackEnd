60c5c2e5ea6c8dc1bccefc83a8605ccd78bd27fb
Subodh Bhandari <subodh.bhandari@sbsolutionsnepal.com>
2022-07-21 12:24:25 +0545

    creating Role entity

---
+++ src/main/java/com/subodh/exampotalbackend/model/Role.java
@@ -0,0 +1,25 @@
+package com.subodh.exampotalbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @author Subodh Bhandari
+ */
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roleName;
+}
